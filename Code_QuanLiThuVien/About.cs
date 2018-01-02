ecc67b2b79df7c27924eee9eb633a638ac71bec8
ngochoa <phamngochoait@gmail.com>
2018-01-02 21:27:15 +0700

    Add File By Ngoc Hoa

--- Code_QuanLiThuVien/About.cs
+++ Code_QuanLiThuVien/About.cs
@@ -17,8 +17,7 @@ namespace WindowsFormsApplication1
             this.labelProductName.Text = AssemblyProduct;
             this.labelVersion.Text = String.Format("Version {0} {0}", AssemblyVersion);
             this.labelCopyright.Text = AssemblyCopyright;
-         //   this.labelCompanyName.Text = AssemblyCompany;
-          //  this.textBoxDescription.Text = AssemblyDescription;
+       
         }
 
         #region Assembly Attribute Accessors
