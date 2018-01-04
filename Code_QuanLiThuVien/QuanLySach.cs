17f412ad34f7e968a99c34a557f60ea5f52218bf
manhhuy243 <34390735+manhhuy243@users.noreply.github.com>
2018-01-04 10:17:24 +0700

    chang buy manh huy

--- Code_QuanLiThuVien/QuanLySach.cs
+++ Code_QuanLiThuVien/QuanLySach.cs
@@ -36,6 +36,37 @@ namespace WindowsFormsApplication1
             // TODO: This line of code loads data into the 'quanLyThuVienDataSet.NhaXuatBan' table. You can move, or remove it, as needed.
             
             // TODO: This line of code loads data into the 'quanLyThuVienDataSet.TheLoai' table. You can move, or remove it, as needed.
+            //Thêm dữ liệu vào cboTinhTrangSach
+            this.cbxTinhTrangSach.Items.Add("Tốt");
+            this.cbxTinhTrangSach.Items.Add("Xấu");
+            this.cbxTinhTrangSach.Items.Add("Bình Thường");
+            this.cbxTinhTrangSach.SelectedItem = "Tốt";
+            this.cbxNamXuatBan.SelectedItem = "2010";
+            this.cbxKhoSach.SelectedItem = "20X30";
+            this.cbxNgonNgu.SelectedItem = "Tiếng việt";
+
+            string sql1 = "Select *from Sach";
+            dataGridViewSach.DataSource = ac.TaoBang(sql1);
+            XoaSach();
+
+            string sql2 = "Select *from TheLoai";
+            dataGridViewTheLoai.DataSource = ac.TaoBang(sql2);
+            XoaTheLoai();
+
+            string sql3 = "Select *from NhaXuatBan";
+            dataGridViewNXB.DataSource = ac.TaoBang(sql3);
+            XoaNXB();
+
+            //lấy tên tác giả
+            cbxTenTacGiaSach.DataSource = LayTenTacGia();
+            cbxTenTacGiaSach.DisplayMember = "TenTacGia";
+            //Lấy tên thể loại
+            cbxTenTheLoaiSach.DataSource = LayTenTheLoai();
+            cbxTenTheLoaiSach.DisplayMember = "TenTheLoai";
+            //Lấy tên NXB
+            cbxTenNXBSach.DataSource = LayTenNXB();
+            cbxTenNXBSach.DisplayMember = "TenNhaXuatBan";
+
             
      }
 }
