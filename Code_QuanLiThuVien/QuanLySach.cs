4b9f5a71dd9de076a6f4caae94a7ea045a4741a4
manhhuy243 <34390735+manhhuy243@users.noreply.github.com>
2018-01-04 12:11:36 +0700

    change buy manhhuy

--- Code_QuanLiThuVien/QuanLySach.cs
+++ Code_QuanLiThuVien/QuanLySach.cs
@@ -293,6 +293,14 @@ namespace WindowsFormsApplication1
             }
         }
 
+        private void txtSoTrangSach_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!Char.IsDigit(e.KeyChar) && !Char.IsControl(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
 
             
      }
