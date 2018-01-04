334b0776d21648449e865da0077b77eabc370609
manhhuy243 <34390735+manhhuy243@users.noreply.github.com>
2018-01-04 10:12:14 +0700

    chang buy manh huy

--- Code_QuanLiThuVien/QuanLySach.cs
+++ Code_QuanLiThuVien/QuanLySach.cs
@@ -14,9 +14,28 @@ namespace WindowsFormsApplication1
 	public QuanLySach()
         {
             InitializeComponent();
-	    AccessData ac = new AccessData();
-	    
         }
-	
+        AccessData ac = new AccessData();
+	private void QuanLySach_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'quanLyThuVienDataSet1.DocGia' table. You can move, or remove it, as needed.
+            // TODO: This line of code loads data into the 'quanLyThuVienDataSet1.Sach' table. You can move, or remove it, as needed.
+            this.sachTableAdapter.Fill(this.quanLyThuVienDataSet1.Sach);
+            // TODO: This line of code loads data into the 'quanLyThuVienDataSet1.Sach' table. You can move, or remove it, as needed.
+            this.sachTableAdapter.Fill(this.quanLyThuVienDataSet1.Sach);
+            // TODO: This line of code loads data into the 'quanLyThuVienDataSet1.TacGia' table. You can move, or remove it, as needed.
+            this.tacGiaTableAdapter1.Fill(this.quanLyThuVienDataSet1.TacGia);
+            // TODO: This line of code loads data into the 'quanLyThuVienDataSet1.NhaXuatBan' table. You can move, or remove it, as needed.
+            this.nhaXuatBanTableAdapter1.Fill(this.quanLyThuVienDataSet1.NhaXuatBan);
+            // TODO: This line of code loads data into the 'quanLyThuVienDataSet1.TheLoai' table. You can move, or remove it, as needed.
+            this.theLoaiTableAdapter1.Fill(this.quanLyThuVienDataSet1.TheLoai);
+            // TODO: This line of code loads data into the 'quanLyThuVienDataSet.TacGia' table. You can move, or remove it, as needed.
+            
+            // TODO: This line of code loads data into the 'quanLyThuVienDataSet.Sach' table. You can move, or remove it, as needed.
+            
+            // TODO: This line of code loads data into the 'quanLyThuVienDataSet.NhaXuatBan' table. You can move, or remove it, as needed.
+            
+            // TODO: This line of code loads data into the 'quanLyThuVienDataSet.TheLoai' table. You can move, or remove it, as needed.
+            
      }
 }
