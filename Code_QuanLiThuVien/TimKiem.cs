d6cadd7dda83b434bedc7991ccb1b1a4c9563c77
ngochoa <phamngochoait@gmail.com>
2018-01-05 11:12:16 +0700

    Add file

--- Code_QuanLiThuVien/TimKiem.cs
+++ Code_QuanLiThuVien/TimKiem.cs
@@ -62,6 +62,60 @@ namespace WindowsFormsApplication1
             dt = db.TaoBang(sql);
             return dt;
         }
+ public static DataTable LayTenTacGia()
+        {
+            string sql;
+            sql = "Select TenTacGia from TacGia ";
+            AccessData db = new AccessData();
+            DataTable dt;
+            dt = db.TaoBang(sql);
+            return dt;
+        }
+        public static DataTable LayTenTheLoai()
+        {
+            string sql;
+            sql = "Select TenTheLoai from TheLoai ";
+            AccessData db = new AccessData();
+            DataTable dt;
+            dt = db.TaoBang(sql);
+            return dt;
+        }
+        public static DataTable LayTenNXB()
+        {
+            string sql;
+            sql = "Select TenNhaXuatBan from NhaXuatBan ";
+            AccessData db = new AccessData();
+            DataTable dt;
+            dt = db.TaoBang(sql);
+            return dt;
+        } 
+        private void cbxTenTacGiaTK_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string tacgia="Select MaTacGia from TacGia where TenTacGia =N'"+cbxTenTacGiaTK.Text.ToString()+"'";
+            string MaTG = Convert.ToString(ac.executeScalar(tacgia));
+            string sql2 = "Select *from Sach where MaTacGia ='" + MaTG + "'";
+            dataGridViewTimKiem.DataSource = ac.TaoBang(sql2);
+        }
+
+        private void cbxTenTLTK_SelectedIndexChanged(object sender, EventArgs e)
+        { //Lấy mã thể loại
+            string theloai = "Select MaTheLoai from TheLoai where TenTheLoai=N'" +cbxTenTLTK.Text.ToString()+ "'";
+            string MaTL = Convert.ToString(ac.executeScalar(theloai));
+            
+            string sql3 = "Select *from Sach where MaTheLoai ='" + MaTL+ "'";
+            dataGridViewTimKiem.DataSource = ac.TaoBang(sql3);
+        }
+
+        private void cbxTenNXBTK_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //Lấy mã NXB
+            string nxb = "Select MaNhaXuaBan from NhaXuatBan where TenNhaXuatBan=N'" + cbxTenNXBTK.Text.ToString() + "'";
+            string MaNXB = Convert.ToString(ac.executeScalar(nxb));
+            
+            string sql4 = "Select *from Sach where MaNhaXuatBan ='" +MaNXB+ "'";
+            dataGridViewTimKiem.DataSource = ac.TaoBang(sql4);
+
+        }
        
 
         private void comboBox1_TextChanged(object sender, EventArgs e)
